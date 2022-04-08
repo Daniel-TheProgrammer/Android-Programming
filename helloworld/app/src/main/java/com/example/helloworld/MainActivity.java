181b514ffa5e1afe5b07bebb7d20f1d951ca94f6
Daniel-TheProgrammer <njid18753@gmail.com>
2022-04-08 06:08:43 +0100

    Added hello world code with Log statements which display code in logcat plane

---
+++ helloworld/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Added Log statements in your app code display messages in the Logcat pane
+        Log.d("MainActivity","Hello World");
+    }
+}
